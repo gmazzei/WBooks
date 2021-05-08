60f2a6ebad07b4c33a0c75d0dca9e9e8ccd13108
Gabriel Leandro Mazzei <gmazzei91@gmail.com>
2021-05-07 22:43:19 -0300

    Refactor - LibraryList and LibraryToolbar created

--- WBooks/Views/Library/LibraryView.swift
+++ WBooks/Views/Library/LibraryView.swift
@@ -10,10 +10,39 @@ import SwiftUI
 struct LibraryView: View {
 
     private struct Constants {
-        static let cellSpacing: CGFloat = 15
         static let scrollViewTopPadding: CGFloat = 32
         static let scrollViewBottomPadding: CGFloat = 5
         static let scrollViewHorizontalPadding: CGFloat = 20 
+    }
+    
+    private var viewModel: LibraryViewModel
+    
+    init(viewModel: LibraryViewModel = LibraryViewModel()) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        ScrollView {
+            LibraryList(viewModel: viewModel)
+                .padding(EdgeInsets(top: Constants.scrollViewTopPadding,
+                                leading: Constants.scrollViewHorizontalPadding,
+                                bottom: Constants.scrollViewBottomPadding,
+                                trailing: Constants.scrollViewHorizontalPadding))
+        }
+        .background(WBooksColors.backgroundColor)
+        .navigationBarTitle("LibrayView.navigationView.title")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            LibraryToolbar()
+        }
+    }
+}
+
+/// List of books
+private struct LibraryList: View {
+    
+    private struct Constants {
+        static let cellSpacing: CGFloat = 15
     }
     
     @ObservedObject private var viewModel: LibraryViewModel
@@ -23,46 +52,44 @@ struct LibraryView: View {
     }
     
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: Constants.cellSpacing) {
-                ForEach(viewModel.cellModels) { cellModel in
-                    NavigationLink(
-                        destination: EmptyView(),
-                        label: {
-                            LibraryCell(viewModel: cellModel)
-                        })
-                        .frame(height: LibraryCell.height)
-                }
-            }
-            .padding(EdgeInsets(top: Constants.scrollViewTopPadding,
-                                leading: Constants.scrollViewHorizontalPadding,
-                                bottom: Constants.scrollViewBottomPadding,
-                                trailing: Constants.scrollViewHorizontalPadding))
-        }
-        .background(WBooksColors.backgroundColor)
-        .navigationBarTitle("LibrayView.navigationView.title")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    print("Alert")
-                }, label: {
-                    WBooksImages.notifications
-                        .renderingMode(.template)
-                })
-            }
-            
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button(action: {
-                    print("Search")
-                }, label: {
-                    WBooksImages.search
-                        .renderingMode(.template)
-                })
+        LazyVStack(spacing: Constants.cellSpacing) {
+            ForEach(viewModel.cellModels) { cellModel in
+                NavigationLink(
+                    destination: EmptyView(),
+                    label: {
+                        LibraryCell(viewModel: cellModel)
+                    })
+                    .frame(height: LibraryCell.height)
             }
         }
-        .onAppear {
-            viewModel.fetchBooks()
+        .onAppear(perform: onLoad)
+    }
+    
+    private func onLoad() {
+        viewModel.fetchBooks()
+    }
+}
+
+/// Buttons for the navigation bar.
+private struct LibraryToolbar: ToolbarContent {
+    
+    var body: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button(action: {
+                print("Alert")
+            }, label: {
+                WBooksImages.notifications
+                    .renderingMode(.template)
+            })
+        }
+        
+        ToolbarItem(placement: .navigationBarTrailing) {
+            Button(action: {
+                print("Search")
+            }, label: {
+                WBooksImages.search
+                    .renderingMode(.template)
+            })
         }
     }
 }
